c5edae5d1341caeb6d94fd8412cda82ae51c23cc
VIVEK KUMAR <vivekkumarprince1@gmail.com>
2024-09-24 10:38:43 +0530

    add vercel

--- config/connectdatabse.js
+++ config/connectdatabse.js
@@ -1,15 +1,16 @@
 // Connect to DB and start server
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/shikshasetu',
-    {
-        //  useNewUrlParser: true,
-        //   useUnifiedTopology: true 
-        })
-        .then(() => console.log('MongoDB connected'))
-        .catch((err) => {
-          console.error('MongoDB connection error:', err);
-          process.exit(1); // Exit the process with a non-zero status code
-        });
+mongoose.connect('mongodb+srv://vivekkumarprince9:SIH%40123@cluster0.bvy5t.mongodb.net/myDatabase?retryWrites=true&w=majority',
+  {
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true
+  })
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => {
+      console.error('MongoDB connection error:', err);
+      process.exit(1); // Exit the process with a non-zero status code
+  });
+
 
     module.exports = mongoose;
